828cb0d4daf868d7d5ebec7f7b176e607b3baf79
Kyle Jackson <kyjackson@csumb.edu>
2021-09-20 01:34:20 -0700

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# CST426-Project-3
-This repository contains all files related to the game I made for CST 426.
--- Source/cst426project3/cst426project3.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "cst426project3.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, cst426project3, "cst426project3" );
- 
